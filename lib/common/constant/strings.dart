5c8a25a62d277ef1348ae69d03fd30194d2bd86b
Risa Herdianto <aputra.zha@gmail.com>
2021-09-02 13:54:08 +0700

    added welcome screen

---
+++ lib/common/constant/strings.dart
@@ -0,0 +1,14 @@
+class Strings {
+  Strings._();
+
+  static const String gin = 'GIN';
+  static const String finans = 'Finans';
+  static const String appName = '$gin $finans';
+  static const String welcomeTo = 'Welcome to';
+  static const String slogan =
+      'Welcome to The Bank of The Future. Manage and track your accounts on the go.';
+
+  static const String email = 'Email';
+
+  static const String next = 'Next';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gin_finans/common/constant/strings.dart';
+import 'package:gin_finans/feature/register/screen/screen_one.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GIN Finans',
+      title: Strings.appName,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GIN Finans'),
-        ),
-      ),
+          primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.blue),
+      home: ScreenOne(),
     );
   }
 }
